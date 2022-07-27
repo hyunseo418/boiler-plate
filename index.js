f4497342442804152e73ab53967555551fa914a6
hs.shin <hs.shin@dnd-lab.com>
2022-07-27 19:28:31 +0900

    :sparkles: 로그인 기능 추가"

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const config = require('./config/key');
 
@@ -9,6 +10,7 @@ const { User } = require('./models/User')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {})
@@ -30,6 +32,34 @@ app.post('/register', (req, res) => {
     })
   }); 
 })
+
+app.post('/login', (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      });
+    }
+    
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 일치하지 않습니다."
+        });
+      }
+      
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess: true, userId: user._id})
+      });
+    });
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
   name: {
@@ -30,6 +33,45 @@ const userSchema = mongoose.Schema({
     type: Number
   }
 })
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+  
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+  
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function(plainPassword, callback) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return callback(err);
+
+    callback(null, isMatch);
+  });
+}
+
+userSchema.methods.generateToken = function(callback) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+  user.token = token;
+  user.save(function(err, user) {
+    if (err) return callback(err);
+
+    callback(null, user);
+  });
+}
 
 const User = mongoose.model('User', userSchema)
 
