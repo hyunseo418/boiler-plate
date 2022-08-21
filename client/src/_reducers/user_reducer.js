a329f79f9aa4695d76b160610187e7f2887811c6
hs.shin <hs.shin@dnd-lab.com>
2022-08-21 19:18:07 +0900

    :sparkles: 회원가입 페이지 제작

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,15 +1,20 @@
 import { createAction, createReducer } from '@reduxjs/toolkit'
 import {
-  LOGIN_USER
+  LOGIN_USER,
+  REGISTER_USER
 } from '../_actions/types';
 
 const login = createAction(LOGIN_USER);
+const register = createAction(REGISTER_USER);
 
 const initialState = {};
 const userReducer = createReducer(initialState, builder => {
   builder
     .addCase(login, (state = {}, action) => {
-      state.loginSuccess = action.payload
+      state.loginSuccess = action.payload;
+    })
+    .addCase(register, (state = {}, action) => {
+      state.register = action.payload;
     })
 });
 
