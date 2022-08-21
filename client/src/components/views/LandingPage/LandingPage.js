bdc4adbc723ff518e089eea0dee4b53ac0561563
hs.shin <hs.shin@dnd-lab.com>
2022-08-21 19:34:38 +0900

    :sparkles: 로그아웃 하기

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,25 @@
 import React, {useEffect} from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 
 function LandingPage() {
+  const navigate = useNavigate();
   useEffect(() => {
     axios.get('/api/hello')
       .then(response => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if (response.data.success) {
+          navigate('/login');
+        } else {
+          alert('로그아웃 하는 데 실패했습니다.');
+        }
+      })
+  }
 
   return ( 
     <div style={{ 
@@ -13,6 +27,9 @@ function LandingPage() {
       , with: '100%', height: '100vh'
     }}>
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>
+        로그아웃
+      </button>
     </div>
   );
 }
